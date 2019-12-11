dcc291b44035140e0ef06cb78bade05f669df0cc
xinxin.li <xinxin.li@tendcloud.com>
2019-12-11 10:48:23 +0800

    防抖节流

---
+++ 1. 节流&防抖.js
@@ -0,0 +1,29 @@
+// 防抖(规定时间内多次触发后，只执行最后一次)
+function debounce(cb, wait) {
+    if (typeof cb === 'function') {
+        let wait = null;
+        if (wait !== null) clearTimeout(wait);
+        wait = setTimeout(cb, wait);
+    }
+}
+
+// 节流(多次触发，只在规定时间后执行)
+function throttle(cb, delay) {
+    if (typeof cb === 'function') {
+        let startTime = Date.now();
+        let timer = null;
+        return function() {
+            const context = this;
+            const ars = arguments;
+            let curTime = Date.now();
+            let remaining = delay - (curTime - startTime);
+            clearTimeout(timer);
+            if (remaining <= 0) {
+                cb.apply(context, ars);
+                startTime = Date.now();
+            } else {
+                timer = setTimeout(cb, remaining);
+            }
+        }
+    }
+}
