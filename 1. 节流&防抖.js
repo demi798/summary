e87db2c5844b769f5b91b4213e2209b1010a18b4
demi <demi798@126.com>
2021-08-11 18:16:29 +0800

    feat: 升级版防抖节流

--- 1. 节流&防抖.js
+++ 1. 节流&防抖.js
@@ -8,22 +8,57 @@ function debounce(cb, wait) {
 }
 
 // 节流(多次触发，只在规定时间后执行)
-function throttle(cb, delay) {
-    if (typeof cb === 'function') {
-        let startTime = Date.now();
-        let timer = null;
-        return function() {
-            const context = this;
-            const ars = arguments;
-            let curTime = Date.now();
-            let remaining = delay - (curTime - startTime);
-            clearTimeout(timer);
-            if (remaining <= 0) {
-                cb.apply(context, ars);
-                startTime = Date.now();
-            } else {
-                timer = setTimeout(cb, remaining);
-            }
+function throttle(cb, delay=0) {
+    if (typeof cb !== 'function') {
+        throw new TypeError('need a function');
+    }
+
+    let timeid = null;
+    let startTime = Date.now();
+
+    return function() {
+        let context = this;
+        let args = arguments;
+        let curTime = Date.now();
+        let remaining = delay - (curTime - startTime);
+        if (timeid) {
+            clearTimeout(timeid);
         }
+        if (remaining <= 0) {
+            cb.apply(context, args);
+            startTime = Date.now();
+            timeid = null;
+        } else {
+            timeid = setTimeout(function() {
+                cb.apply(context, args);
+            }, wait);
+        }
+    }
+}
+
+// 升级版防抖
+// 升级点：
+// 1、 明确 this 指向； 
+// 2、 继承函数传参
+// 3、 增加返回值
+function upDebounce(cb, wait=0) {
+    if (typeof cb !== 'function') {
+        throw new TypeError('need a function arguments')
+    }
+    let timeid = null;
+    let result;
+    
+    return function() {
+        let context = this;
+        let args = arguments;
+
+        if (timeid) {
+            clearTimeout(timeid);
+        }
+        timeid = setTimeout(function() {
+            result = cb.apply(context, args);
+        }, wait);
+        
+        return result;
     }
 }
